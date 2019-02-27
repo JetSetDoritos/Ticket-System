f85c75f46a28a5f989fbeaab3e0e439c940c5c14
Mason <masonjuarez@pm.me>
2019-02-27 02:52:20 -0800

    reads from db

--- src/App.js
+++ src/App.js
@@ -4,21 +4,50 @@ import './App.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import Tabs from './Tabs';
 import PropTypes from 'prop-types';
+import firebase from "firebase";
+
 
 
 var data = require('./data/database.json');
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBKTvYuchVUVMBETWFvRsO56eP_7yIWqCs",
+    authDomain: "ticket-system-d1de5.firebaseapp.com",
+    databaseURL: "https://ticket-system-d1de5.firebaseio.com",
+    projectId: "ticket-system-d1de5",
+    storageBucket: "ticket-system-d1de5.appspot.com",
+    messagingSenderId: "441428437256"
+  };
+  firebase.initializeApp(config);
+
+  /*
+  // Get a reference to the database service
+  var database = firebase.database();
+
+   return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // ...
+  });*/
+
 console.log(data);
 
 var dataset = [];
-var datasize = 0;
+var datasize = 8;
 var dummy;
-for(dummy in data){
+/*for(dummy in data){
   datasize++;
 }
 for (var x = 1; x < datasize+1; x++){
   console.log(x)
   dataset.push({'id': data[x.toString()].id, 'name': data[x.toString()].name, 'hash': data[x.toString()].hash});
+}*/
+
+for(var x = 1; x < datasize+1; x++){
+  firebase.database().ref(x).once('value').then(function(snapshot) {
+    dataset.push({'id': snapshot.val().id, 'name': snapshot.val().name, 'hash': snapshot.val().hash});
+    // ...
+  });
 }
 
 console.log(dataset);
@@ -47,15 +76,22 @@ class App extends Component {
       <div className="App">
         
         <Tabs>
-        <div label="Gator">
-          See ya later, <em>Alligator</em>!
-        </div>
-        <div label="Croc">
-          After &apos;while, <em>Crocodile</em>!
+        <div label="Tickets">
+          List of tickets
           <BootstrapTable keyField='id' data={ dataset } columns={ columns }  />
         </div>
-        <div label="Sarcosuchus">
-          Nothing to see here, this tab is <em>extinct</em>!
+        <div label="Send">
+          Send Tickets
+          <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+        </div>
+        <div label="Admin">
+          to be added
         </div>
       </Tabs>
 
